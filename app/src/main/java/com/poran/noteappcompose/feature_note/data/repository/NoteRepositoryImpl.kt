f1ee920a3bc2cf241ef9ffe1505f9c38c570a4f4
ridmik <poran.cse@gmail.com>
2021-10-02 02:16:35 +0600

    created note dao interface

---
+++ app/src/main/java/com/poran/noteappcompose/feature_note/data/repository/NoteRepositoryImpl.kt
@@ -0,0 +1,26 @@
+package com.poran.noteappcompose.feature_note.data.repository
+
+import com.poran.noteappcompose.feature_note.data.data_source.NoteDao
+import com.poran.noteappcompose.feature_note.domain.model.Note
+import com.poran.noteappcompose.feature_note.domain.repository.NoteRepository
+import kotlinx.coroutines.flow.Flow
+
+class NoteRepositoryImpl(
+    private val noteDao: NoteDao
+): NoteRepository {
+    override fun getNotes(): Flow<List<Note>> {
+        return noteDao.getNotes()
+    }
+
+    override suspend fun getNoteById(id: Int): Note? {
+        return noteDao.getNoteById(id)
+    }
+
+    override suspend fun insertNote(note: Note) {
+        noteDao.insertNote(note)
+    }
+
+    override suspend fun deleteNote(note: Note) {
+        noteDao.deleteNote(note = note)
+    }
+}
